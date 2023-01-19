6699984e0cb529e03533ac3b2dff342b6a8ed8ca
Augusto Busetti <gugu.busetti@gmail.com>
2023-01-19 02:11:26 -0300

    Update bot.py

--- 005 Banco de dados/bot.py
+++ 005 Banco de dados/bot.py
@@ -1,1 +1,20 @@
+import discord
+import json
+from discord.ext import commands
 
+client = commabnds.Bot(command_prefix='Prefix')
+
+@client.event
+async def on_ready():
+  print('Olá mundo')
+  
+  
+  with open('config.js') as e: # armazenando com Json
+    config = json.load(e)
+    TOKEN = config['token']
+  
+  with open('token.txt','r') as archive:
+    TOKEN = archive.read()
+    
+   
+client.run('TOKEN')
