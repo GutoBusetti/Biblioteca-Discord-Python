f0a9858d9307acbbdd3c2e00410b62f6b3884ce2
Augusto Busetti <gugu.busetti@gmail.com>
2023-01-19 01:42:35 -0300

    Update bot.py

--- Iniciando o bot/bot.py
+++ Iniciando o bot/bot.py
@@ -1,1 +1,10 @@
+import discord # importações das libs usadas, aqui no caso a do Discord
+fro discord.ext import commands
 
+client = commands.Bot()
+
+@client.event
+async def on_ready():
+  print('Olá mundo')
+  
+  client.run('Seu token') # token do bot
